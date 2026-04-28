4afa88f1e8b70940398a7612803e2a6cec3cada7
ShubhamSharma <shubham44998@gmail.com>
2026-04-28 11:52:50 +0530

    updating code.

---
+++ src/main/java/com/patchly/utils/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.patchly.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * ErrorResponse class represents the structure of the error response returned to the client.
+ * It contains a timestamp, an error message, and an error code.
+ */
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String code;
+    private String path;
+}
